75df712914c081d7f05be1fab42997e6d7280860
Oscar Zambrano <zambrano20407@gmail.com>
2024-01-31 00:15:54 -0500

    feat: password encryption

--- src/controllers/usuarios/usuariosController.ts
+++ src/controllers/usuarios/usuariosController.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
-
+import bcrypy from 'bcrypt';
+import jwt from 'jsonwebtoken';
 const prisma = new PrismaClient();
 
 const getMethod = async(req: Request, res: Response) => {
@@ -27,8 +28,16 @@ const getMethodById = async(req: Request, res: Response) => {
 const postMethod = async(req: Request, res: Response) => {
     try {
         const {body} = req;
+        //primero que no exista en la base de datos
+        const exists = await prisma.usuarios.findUnique({where: {correo: body.correo}});
+        if (exists) {
+            return res.status(400).json({message: 'El correo electronico ya extiste'});
+        }
+
+        const hashedPassword = await bcrypy.hashSync(body.clave, 10);
+        body.clave = hashedPassword;
         const result = await prisma.usuarios.create({data: body});
-        return res.status(200).json(result);
+        return res.status(201).json(result);
     } catch (error) {
         console.log("error::controller::usuarios", error);
         return res.status(500).json(error);
